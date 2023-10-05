6a37415601650de890fbb770e50cd21ea834d417
kai <flynn98721@gmail.com>
2023-10-05 13:27:03 -0400

    loading page full width

--- react/src/components/LoadingPage.tsx
+++ react/src/components/LoadingPage.tsx
@@ -2,7 +2,7 @@ import { Flex, Spinner } from "@chakra-ui/react";
 
 const LoadingPage = () => {
   return (
-    <Flex h="90vh" w="100vw" justify="center" align="center">
+    <Flex h="90vh" justify="center" align="center">
       <Spinner size="xl" speed="0.8s" thickness="4px" />
     </Flex>
   );
