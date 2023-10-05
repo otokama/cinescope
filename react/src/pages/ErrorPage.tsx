a4f7e27dca426a091aacd5b8ac4286b2b9543842
kai <flynn98721@gmail.com>
2023-10-05 10:01:18 -0400

    error page styling in small screen

--- react/src/pages/ErrorPage.tsx
+++ react/src/pages/ErrorPage.tsx
@@ -10,8 +10,8 @@ const ErrorPage = () => {
     <>
       <Navbar onSearch={() => {}} />
       <Flex h="100vh" w="100vw" justify="center" align="center">
-        <HStack h="full">
-          <Image src={fullMoonImg} alt="" boxSize={48} />
+        <HStack  spacing={{base: 3, md: 5}}>
+          <Image src={fullMoonImg} alt="" boxSize={{base:16, sm: 20, md: 32, lg: 48}} />
           <VStack align="start">
             {isRouteErrorResponse(error) ? (
               <>
