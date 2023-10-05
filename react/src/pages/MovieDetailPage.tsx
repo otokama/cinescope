bc9ee3ae33f37b7d22d2293990ce51cc1bbf7262
kai <flynn98721@gmail.com>
2023-10-05 10:01:34 -0400

    movie detail page error handling

--- react/src/pages/MovieDetailPage.tsx
+++ react/src/pages/MovieDetailPage.tsx
@@ -1,14 +1,15 @@
 import { useParams } from "react-router-dom";
 import LoadingPage from "../components/LoadingPage";
 import useMovieDetail from "../hooks/movies/useMovie";
+import { useNavigate } from "react-router-dom";
 
 const MovieDetailPage = () => {
   const { id } = useParams();
   const { data: movieDetail, isLoading, error } = useMovieDetail(parseInt(id!));
-
+  const navigate = useNavigate();
   if (isLoading) return <LoadingPage />;
 
-  if (error || !movieDetail) throw error;
+  if (error || !movieDetail) navigate("*");
 
   return <div>MovieDetailPage</div>;
 };
